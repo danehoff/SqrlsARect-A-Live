facee605a0f8d86655ba905e9f6593c6f0bb9d40
Dane Hoffman <danehoff@ksu.edu>
2020-10-09 16:17:38 -0500

    Add project files.

---
+++ SqrlsARect-A-Live/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SqrlsARect_A_Live
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            ///Square s1 = new Square();
+            SquareSquare s1 = new SquareSquare();
+
+            Console.WriteLine("Calling the funtion with Rectangle.");
+            UserOfRectangle(r1);
+
+            Console.WriteLine("Now, calling the function with a Square.");
+            UserOfRectangle(s1);
+            Console.ReadLine();
+
+        }
+
+        static void UserOfRectangle(Rectangle r)
+        {
+
+            r.Height = 5;
+            r.Width = 4;
+
+            if(r.CalculateArea() != 20)
+            {
+                throw new Exception("Bad Area!");
+            }
+            else
+            {
+
+                Console.Write("For the Shape with sides: " + r.Height + " by " + r.Width + ", ");
+                Console.WriteLine("The Area is: " + r.CalculateArea());
+
+            }
+
+        }
+
+    }
+}
---
+++ SqrlsARect-A-Live/Rectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqrlsARect_A_Live
+{
+    class Rectangle
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public int CalculateArea()
+        {
+
+            return Height * Width;
+
+        }
+    }
+
+    class Square : Rectangle
+    {
+
+
+
+    }
+
+    class SquareSquare : Rectangle
+    {
+
+        public override int Height
+        {
+
+            get { return base.Height; }
+            set { base.Height = base.Width = value; }
+
+        }
+
+        public override int Width
+        {
+
+            get { return base.Height; }
+            set { base.Height = base.Width = value; }
+
+        }
+
+    }
+
+}
